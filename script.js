61651a1e103a9851db9d3c8b4e8118bea86f61eb
Omaima Ali Khan <omaimaak4443500@gmail.com>
2024-10-13 17:28:35 +0500

    Defining the object Pet

---
+++ script.js
@@ -0,0 +1,8 @@
+// Function to define the Pet object with properties and methods
+function Pet(name, type, age, happiness, hunger) {
+    this.name = name;
+    this.type = type;
+    this.age = age;
+    this.happiness = happiness;
+    this.hunger = hunger;
+}
